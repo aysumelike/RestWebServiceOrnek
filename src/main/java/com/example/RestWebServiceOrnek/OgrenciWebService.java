2f7d2faa5b12a3783f18e94ffb9a3a60488b7c4a
aysumelike <aysumelikegumus@gmail.com>
2024-01-16 20:22:58 +0300

    web service

---
+++ src/main/java/com/example/RestWebServiceOrnek/OgrenciWebService.java
@@ -0,0 +1,24 @@
+package com.example.RestWebServiceOrnek;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/ogrenci")
+public class OgrenciWebService {
+
+	public record Ogrenci(String adSoyad, String numara) {
+	};
+
+	private static final List<Ogrenci> OGRENCI_LISTESI = new ArrayList<>();
+	static {
+		OGRENCI_LISTESI.add(new Ogrenci("Ali Veli", "1"));
+		OGRENCI_LISTESI.add(new Ogrenci("Ayşe Fatma", "2"));
+		OGRENCI_LISTESI.add(new Ogrenci("Mehmet Emin", "3"));
+	}
+
+	@GetMapping("/")
+	public List<Ogrenci> listele() {
+		return OGRENCI_LISTESI;
+	}
+}
---
+++ src/main/java/com/example/RestWebServiceOrnek/RestWebServiceOrnekApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestWebServiceOrnek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebServiceOrnekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebServiceOrnekApplication.class, args);
+	}
+
+}
